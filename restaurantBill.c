b899257f380fe126d608407e843d0e6e0ce18aa4
AndrewJohnValdez <andrewjohnv@cpp.edu>
2022-10-20 22:21:19 -0700

    Created Outline for restaurantBill.c

---
+++ restaurantBill.c
@@ -0,0 +1,37 @@
+//restaurantBill.c
+/*
+Program Requirements:
+Values are to be passed as arguments (argc & argv)
+Compute tax and tip on restaurant bill
+    tax and tip are both percentages as command line arguments
+        California Tax = 9.75%
+        Tip: 5%, 10%, 15%
+ask for patron name?
+Meal cost added up
+Total cost overall
+
+*/
+
+/*
+#include <stdio.h>
+#include <conio.h>
+*/
+
+/*
+void main() {
+
+//Print list 
+//Print the list of costs for salad, soup, sandwich, and pizza, listing them as choices to be ordered for the receipt
+//possibly make a main menu for the options
+
+//Possible randomizer for choosing the type of food wanted, 1 out of the 4
+
+
+//Method to calculate the tax 
+
+
+//Method to calculate the tip by three choices that are displayed and based on user input
+
+
+}
+*/
